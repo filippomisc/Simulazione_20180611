6af42906028c6929ca5955ce6ed5d4ea7103a0dd
mastr <mastr@172.22.43.182>
2018-07-22 12:37:34 +0200

    v1.0
    -create le classi javaBean

---
+++ src/it/polito/tdp/ufo/model/Neighbor.java
@@ -0,0 +1,69 @@
+package it.polito.tdp.ufo.model;
+
+public class Neighbor {
+	private String state1;
+	private String state2;
+	
+	
+	public Neighbor(String state1, String state2) {
+		super();
+		this.state1 = state1;
+		this.state2 = state2;
+	}
+
+
+	public String getState1() {
+		return state1;
+	}
+	public void setState1(String state1) {
+		this.state1 = state1;
+	}
+	public String getState2() {
+		return state2;
+	}
+	public void setState2(String state2) {
+		this.state2 = state2;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((state1 == null) ? 0 : state1.hashCode());
+		result = prime * result + ((state2 == null) ? 0 : state2.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Neighbor other = (Neighbor) obj;
+		if (state1 == null) {
+			if (other.state1 != null)
+				return false;
+		} else if (!state1.equals(other.state1))
+			return false;
+		if (state2 == null) {
+			if (other.state2 != null)
+				return false;
+		} else if (!state2.equals(other.state2))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Neighbor [state1=" + state1 + ", state2=" + state2 + "]";
+	}
+	
+	
+
+}
